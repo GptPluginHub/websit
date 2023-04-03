80babdc2de5487bc5480d75314b263592b455f0b
fengjian <fengjian001@chinasofti.com>
2023-04-03 18:06:47 +0800

    修改

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,13 +4,8 @@
       <el-col :span="12" :xs="24">
         <el-form ref="form" @submit.prevent>
           <el-form-item>
-            <el-input
-              v-model="data.form.text"
-              placeholder="请输入你要搜索的内容"
-              @keyup.enter="handleSearch"
-            >
-              <template #append> <el-button :icon="data.Search" /> </template
-            ></el-input>
+            <el-input v-model="data.form.text" placeholder="请输入你要搜索的内容" @keyup.enter="handleSearch">
+              <template #append> <el-button @click="handleSearch" :icon="data.Search" /> </template></el-input>
           </el-form-item>
           <el-form-item>
             <el-checkbox-group v-model="data.form.check">
@@ -24,18 +19,9 @@
   </div>
   <div class="content-view">
     <el-row :gutter="20">
-      <el-col
-        :xs="24"
-        :sm="6"
-        :md="4"
-        v-for="(item, index) in data.listCard"
-        :key="index"
-      >
+      <el-col :xs="24" :sm="6" :md="4" v-for="(item, index) in data.listCard" :key="index">
         <el-card :body-style="{ padding: '0px' }" class="card-view">
-          <img
-            src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
-            class="card-image"
-          />
+          <img :src="image" class="card-image" />
           <div style="margin: 10px">
             <span>{{ item.name }}</span>
           </div>
@@ -43,17 +29,11 @@
             <span class="card-des">{{ item.des }}</span>
           </div>
           <div style="margin: 0 10px">
-            <el-rate v-model="item.star" size="small" allow-half disabled/>
+            <el-rate v-model="item.star" size="small" allow-half disabled />
           </div>
           <div style="margin: 10px">
-            <el-tag
-              v-for="(item2, index2) in item.tag"
-              :key="index2"
-              size="small"
-              type="success"
-              class="ml-2"
-              >{{ item2 }}</el-tag
-            >
+            <el-tag v-for="(item2, index2) in item.tag" :key="index2" size="small" type="success" class="ml-2">{{ item2
+            }}</el-tag>
           </div>
         </el-card>
       </el-col>
@@ -62,14 +42,16 @@
 </template>
 <script>
 import { reactive } from "vue";
-import { Search } from "@element-plus/icons-vue";
+import { ElMessage } from 'element-plus'
+import { Search } from '@element-plus/icons-vue'
+import image from "../../assets/image/0.webp"
 export default {
   setup() {
     const data = reactive({
       Search,
       form: {
-          text: '',
-          check: []
+        text: '',
+        check: []
       },
       listCard: [
         {
@@ -117,10 +99,15 @@ export default {
       ],
     });
     const handleSearch = () => {
-      
+      ElMessage({
+        showClose: true,
+        message: '搜索中',
+        type: 'warning',
+      })
     };
     return {
       data,
+      image,
       handleSearch,
     };
   },
@@ -131,18 +118,23 @@ export default {
   margin-top: 50px;
   padding: 0 20px;
 }
+
 .content-view {
   padding: 20px;
 }
+
 .card-view {
   margin-bottom: 20px;
+
   .card-image {
     width: 100%;
   }
+
   .card-des {
     color: #888888;
     font-size: 14px;
   }
+
   .ml-2 {
     margin: 0 5px 5px 0px;
   }
